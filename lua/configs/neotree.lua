000725e29619ff60421f293ec6c32de20cfd0098
sharad <paponahmedsharad@gmail.com>
2023-01-26 17:42:10 +0600

    1px padding added in neotree

--- lua/configs/neotree.lua
+++ lua/configs/neotree.lua
@@ -1,77 +1,84 @@
 local status_ok, neotree = pcall(require, "neo-tree")
 if not status_ok then
-  return
+	return
 end
 
 --> setup
-neotree.setup {
-  close_if_last_window = true,
-  popup_border_style = "rounded",
-  enable_diagnostics = false,
-  default_component_configs = {
-    indent = {
-      padding = 0,
-      with_expanders = false,
-    },
-    icon = {
-      folder_closed = "",
-      folder_open = "",
-      folder_empty = "",
-      default = "",
-    },
-    git_status = {
-      symbols = {
-        added = "",
-        deleted = "",
-        modified = "",
-        renamed = "➜",
-        untracked = "★",
-        ignored = "◌",
-        unstaged = "✗",
-        staged = "✓",
-        conflict = "",
-      },
-    },
-  },
-  window = {
-    position = "left",
-    width = 25,
-    mapping_options = {
-      noremap = true,
-      nowait = true,
-    },
-    mappings = {
-      ["o"] = "open",
-      ["w"] = "open_with_window_picker",
-    },
-  },
-  filesystem = {
-    filtered_items = {
-      visible = false,
-      hide_dotfiles = true,
-      hide_gitignored = false,
-      hide_by_name = {
-        ".DS_Store",
-        "thumbs.db",
-        "node_modules",
-        "__pycache__",
-      },
-    },
-    follow_current_file = true,
-    hijack_netrw_behavior = "open_current",
-    use_libuv_file_watcher = true,
-  },
-  git_status = {
-    window = {
-      position = "float",
-    },
-  },
-  event_handlers = {
-    {
-      event = "neo_tree_buffer_enter",
-      handler = function(_)
-        vim.opt_local.signcolumn = "auto"
-      end,
-    },
-  },
-}
+neotree.setup({
+	close_if_last_window = true,
+	popup_border_style = "rounded",
+	enable_diagnostics = false,
+	default_component_configs = {
+		indent = {
+			padding = 1,
+			indent_marker = "│",
+			last_indent_marker = "└",
+			highlight = "NeoTreeIndentMarker",
+			-- expander config, needed for nesting files
+			with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
+			expander_collapsed = "",
+			expander_expanded = "",
+			expander_highlight = "NeoTreeExpander",
+		},
+		icon = {
+			folder_closed = "",
+			folder_open = "",
+			folder_empty = "",
+			default = "",
+		},
+		git_status = {
+			symbols = {
+				added = "",
+				deleted = "",
+				modified = "",
+				renamed = "➜",
+				untracked = "★",
+				ignored = "◌",
+				unstaged = "✗",
+				staged = "✓",
+				conflict = "",
+			},
+		},
+	},
+	window = {
+		position = "left",
+		width = 25,
+		mapping_options = {
+			noremap = true,
+			nowait = true,
+		},
+		mappings = {
+			["o"] = "open",
+			["w"] = "open_with_window_picker",
+		},
+	},
+	filesystem = {
+		filtered_items = {
+			visible = false,
+			hide_dotfiles = true,
+			hide_gitignored = false,
+			hide_by_name = {
+				".DS_Store",
+				"thumbs.db",
+				"node_modules",
+				"__pycache__",
+			},
+		},
+		follow_current_file = false,
+		hijack_netrw_behavior = "open_current",
+		use_libuv_file_watcher = true,
+	},
+	git_status = {
+		window = {
+			position = "float",
+		},
+	},
+	event_handlers = {
+		{
+			event = "neo_tree_buffer_enter",
+			handler = function(_)
+				vim.opt_local.signcolumn = "auto"
+			end,
+		},
+	},
+})
